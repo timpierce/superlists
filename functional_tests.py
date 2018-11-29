9b74ea0f3022106351fe5b1b58b9b17c425fb7b9
Tim Pierce <timpierce.py@gmail.com>
2018-11-29 09:55:04 -0600

    added form post processing for template to pass FT

--- functional_tests.py
+++ functional_tests.py
@@ -29,17 +29,28 @@ class NewVisitorTest(unittest.TestCase):
         # She enters "Buy peacock feathers"
         inputbox.send_keys(' Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # The text box prompts her to add another item
         # She enters "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
         # Now both items are in the list
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
         # She wonders if the website will remember her list so she goes directly to the website url again.
         # The to-do list still exists
         self.fail("Finish the test.")
