41e98c78149fe080bd55773b3566a7b446581a89
Tim Pierce <timpierce.py@gmail.com>
2018-12-05 10:47:39 -0600

    add item to existing list functionality

--- lists/views.py
+++ lists/views.py
@@ -6,18 +6,21 @@ from lists.models import Item, List
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+def view_list(request, list_id):
+    list_ = List.objects.get(pk=list_id)
+    return render(request, 'list.html', {'list': list_})
 
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{list_.id}/')
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(pk=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -21,6 +21,7 @@ from lists import views
 urlpatterns = [
     path(r'', views.home_page, name='home'),
     path(r'lists/new', views.new_list, name='new_list'),
-    path(r'lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
     path('admin/', admin.site.urls),
 ]
